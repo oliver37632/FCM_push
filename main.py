8fee8bc411e67a53e578877250b0ed5a8100e228
김승진 <klli1naver@gmail.com>
2021-12-29 19:21:01 +0900

    created:: main

---
+++ main.py
@@ -0,0 +1,26 @@
+from pyfcm import FCMNotification
+from flask import Flask, request
+
+app = Flask(__name__)
+
+APIKEY = "AAAAGwXE9D0:APA91bHZncDSGBtGqifCp4mz8dF-3729qKt0GWcmu5mBcL1oESAUIAuhz-infW0YrxUru3PByJ7jnKURrTCWJteostlrCZbvR1nbG-CDDlKt_YtfG3OzRRoc4XtILNJqts1cEqBoelHJ"
+
+push_service = FCMNotification(APIKEY)
+
+
+@app.route('/push', methods=['POST'])
+def sendMessage():
+    title = request.json("title")
+    content = request.json("content")
+    token = request.json("token")
+
+    message = {
+        "title": title,
+        "content": content
+    }
+    result = push_service.notify_single_device(registration_id=token, data_message=message)
+    print(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
